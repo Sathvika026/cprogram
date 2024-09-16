fe8544b9930f8c30e0b797938d7f0493e93d5aae
Sathvika026 <sathvikasat26@gmail.com>
2024-09-16 15:14:08 +0530

    Create numpyramid.c

---
+++ numpyramid.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,b,n=6;
+for(a=1;a<=10;a++)
+{
+if(a==1||a==2||a==4||a==7)
+{
+printf("\n");
+for(b=1;b<=n;b++)
+printf(" ");
+n=n-2;
+}
+printf("%4d",a);
+}
+}
